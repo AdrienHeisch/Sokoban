2a258c0bfb981ecc077e75b035db48006a68a557
AdrienHeisch <adrienheisch@orange.fr>
2018-06-22 16:23:17 +0200

    fix assets names

--- scripts/app/gameObjects/GameObject.js
+++ scripts/app/gameObjects/GameObject.js
@@ -58,7 +58,7 @@ define([
          * @param {String} [assetName] 
          */
         setImage(assetName) {
-            if (!assetName) assetName = this.constructor.name.charAt(0).toUpperCase() + this.constructor.name.slice(1);
+            if (!assetName) assetName = this.constructor.name.toLowerCase();
 
             var path = "assets/" + assetName;
 
